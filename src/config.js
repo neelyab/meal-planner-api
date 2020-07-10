4ea1131732e939d8fcf307877e20e732f85df18f
Amanda <neelyab@vcu.edu>
2020-07-10 17:11:54 -0400

    added client origin

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN ||  'https://meal-planner-app-eight.vercel.app/',
+    CLIENT_ORIGIN:  'https://meal-planner-app-eight.vercel.app/',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://amanda:amanda@localhost/meal_planner',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://amanda:amanda@localhost/meal_planner_test",
     JWT_SECRET: process.env.JWT_SECRET,
